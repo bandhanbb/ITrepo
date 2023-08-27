41e1a38c521de5e1ca2d0ee90d66ab41902b39f6
Gourab Banerjee <gourabbanerjee365@outlook.com>
2023-08-27 21:40:40 +0530

    from master

--- db2.java
+++ db2.java
@@ -2,3 +2,9 @@ hnnln
 this is from locally
 this is from gui
 hluihlh;lh;lkh;l.n.lk
+ygkjg,bkbkbljbln
+jhjfvjvlb;obn;lk/bnjbbln/n/n  /1./ 
+
+mn l/ / l  m/.   /m;m/l lnm l;juigkukfv .kb./k;bkujhvmnfoo = new bnjbbln/n/n  /1./ 
+
+mn l/ / l  m/.   /m;m/l lnm l;juigkukfv .kb./k;bkujhvmn ();
